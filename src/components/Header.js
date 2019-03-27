a8d098adc4327165975417c1c9d4346a98d595f6
Ayron Felipe <ayron41@gmail.com>
2019-03-27 16:48:13 -0300

    Refatoração minimizaçao sidebar
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ export default class Header extends React.Component{
 
     constructor(props){
         super(props);
-        this.state = {toggle_topbar: false, topbar_open: 1, toggle_sidebar: false, nav_open: 1};
+        this.state = {toggle_topbar: false, topbar_open: 1, toggle_sidebar: false, nav_open: 1, minimize_sidebar: false, mini_sidebar: 1};
     }
 
     toggleSidebar = () => {
         let toggle = $('.sidenav-toggler');
-        console.log(this.state.nav_open);
+
         if (this.state.nav_open == 1) {
             $('html').removeClass('nav_open');
             toggle.removeClass('toggled');
@@ -38,33 +38,28 @@ export default class Header extends React.Component{
     }
 
     minimizeSidebar = () => {
-        let minimize_sidebar = false;
-        let mini_sidebar = 0;
-    
-        if (!minimize_sidebar) {
-            let minibutton = $('.toggle-sidebar');
-            if ($('.wrapper').hasClass('sidebar_minimize')) {
-                mini_sidebar = 1;
-                minibutton.addClass('toggled');
-                minibutton.html('<i class="icon-options-vertical"></i>');
-            }
-    
-            minibutton.on('click', function () {
-                if (mini_sidebar == 1) {
-                    $('.wrapper').removeClass('sidebar_minimize');
-                    minibutton.removeClass('toggled');
-                    minibutton.html('<i class="icon-menu"></i>');
-                    mini_sidebar = 0;
-                } else {
-                    $('.wrapper').addClass('sidebar_minimize');
-                    minibutton.addClass('toggled');
-                    minibutton.html('<i class="icon-options-vertical"></i>');
-                    mini_sidebar = 1;
-                }
-                $(window).resize();
-            });
-            minimize_sidebar = true;
+        let minibutton = $('.toggle-sidebar');
+
+        if ($('.wrapper').hasClass('sidebar_minimize')) {
+            this.setState({mini_sidebar: 0});
+            minibutton.addClass('toggled');
+            minibutton.html('<i class="icon-options-vertical"></i>');
         }
+
+        if (this.state.mini_sidebar == 1) {
+            $('.wrapper').removeClass('sidebar_minimize');
+            minibutton.removeClass('toggled');
+            minibutton.html('<i class="icon-menu"></i>');
+            this.setState({mini_sidebar: 0});
+        } else {
+            $('.wrapper').addClass('sidebar_minimize');
+            minibutton.addClass('toggled');
+            minibutton.html('<i class="icon-options-vertical"></i>');
+            this.setState({mini_sidebar: 1});
+        }
+        $(window).resize();
+        this.setState({minimize_sidebar: true});
+
     }
 
     componentDidMount(){
