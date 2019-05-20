75179003f74ef6fdc14db234aae78d90e78aad9d
Ayron Felipe <ayron41@gmail.com>
2019-05-20 15:03:59 -0300

    Alteração no Componente de busca de clientes na pré-venda
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/SearchCliente.js
+++ src/components/SearchCliente.js
@@ -1,59 +1,209 @@
 import React from 'react';
+import axios from 'axios';
+import MaskedInput from 'react-text-mask';
+
 
 const PAGE = 1;
+
+const inputStyle = {
+    textTransform: 'uppercase'
+}
+
 
 export default class SearchCliente extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { cliente: '', readonly: false };
+        this.state = {clientes: [], readonly: false, showCodigo: true, showCPFCNPJ: false, showNome: false, };
         this.input = React.createRef();
     }
 
     searchCliente = () => {
-        swal({
-            text: "Código ou CPF ou CNPJ do cliente sem formatação",
-            content: "input",
-            button: {
-                text: "Procurar!",
-                closeModal: false,
-            },
-        })
-            .then((name) => {
-                if (!name) throw null;
-                this.setState({ cliente: name });
-                return fetch(`http://api.nortelink.com.br/api/v1/clientes/${name}`, {
-                    method: `GET`,
-                    headers: { 'Authorization': `Bearer ${localStorage.token}` },
-                });
-            })
-            .then((results) => {
-                return results.json();
-            })
-            .then((json) => {
-                if (json.status == 404) {
-                    return swal("Cliente não encontrado");
-                } else {
-                    let cliente_encontrado_list = []
-                    cliente_encontrado_list.push(json);
-                    const cliente_encontrado = cliente_encontrado_list[0];
-                    swal({
-                        title: "Cliente encontrado!",
-                        text: cliente_encontrado.nome,
-                    });
-                    this.input.current.value = cliente_encontrado.id;
-                    this.props.onChange(this.input.current.name, this.input.current.value);
-                }
-            });
+        $('#modal-cliente').modal('show');
     }
 
     changeHandler = (e) => {
         this.props.onChange(this.input.current.name, this.input.current.value);
     }
 
+    changeDesejo = (e) => {
+        if (e.target.value == 'id') {
+            this.setState({
+                showCodigo: true,
+                showCPFCNPJ: false,
+                showNome: false,
+            });
+        } else if (e.target.value == 'cpfcnpj') {
+            this.setState({
+                showCodigo: false,
+                showCPFCNPJ: true,
+                showNome: false,
+            });
+        } else {
+            this.setState({
+                showCodigo: false,
+                showCPFCNPJ: false,
+                showNome: true,
+            });
+        }
+    }
+
+    renderOptionsBusca = () => {
+        if (this.state.showCodigo) {
+            return (
+                <div className="form-group">
+                    <label htmlFor="codigo">Código do cliente</label>
+                    <input type="text" placeholder="Código do Cliente" name="codigo" id="codigo" className="form-control" />
+                </div>
+            );
+        } else if (this.state.showCPFCNPJ) {
+            return (
+                <div className="form-group">
+                    <label htmlFor="cpf_cnpj">CPF ou CNPJ do cliente (sem formatação)</label>
+                    <MaskedInput
+                        mask={[/\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/,]}
+                        guide={false}
+                        placeholder="CPF ou CNPJ"
+                        className="form-control"
+                        name="cpf_cnpj"
+                        id="cpf_cnpj"
+                        required />
+                </div>
+            );
+        } else {
+            return (
+                <div className="form-group">
+                    <label htmlFor="nome">Nome do cliente</label>
+                    <input type="text" placeholder="Nome do Cliente" name="nome" id="nome" className="form-control" style={inputStyle} />
+                </div>
+            );
+        }
+    }
+
+    handleClienteClick = () => {
+        let order = '';
+        let value = '';
+        if (this.state.showCodigo) {
+            order = 'id';
+            value = $('#codigo').val().toUpperCase();
+        } else if (this.state.showCPFCNPJ) {
+            order = 'cpf_cnpj';
+            value = $('#cpf_cnpj').val().toUpperCase();
+        } else {
+            order = 'nome';
+            value = $('#nome').val().toUpperCase();
+        }
+        this.setState({ carregaInfo: true });
+        axios({
+            url: `http://api.nortelink.com.br/api/v1/clientes/`,
+            method: `get`,
+            headers: {
+                'Authorization': `Bearer ${localStorage.token}`
+            },
+            params: {
+                page: PAGE,
+                order: order,
+                value: value
+            }
+        })
+        .then((res) => {
+            this.setState({ clientes: res.data });
+        })
+        .catch((error) => {
+            this.setState({ clientes: '' });
+            swal("Erro!", `${error.response.data.message}`, {
+                icon: "error",
+                buttons: {
+                    confirm: {
+                        className: 'btn btn-danger'
+                    }
+                },
+            }).then(() => {
+                verifyToken(error.response.data.message);
+            });
+        });
+    }
+
+    renderClientesEncontrados = () => {
+        if (this.state.clientes.length >= 1) {
+            return(
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Código</th>
+                                <th>Nome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.clientes.map((cliente) => 
+                                <tr key={cliente.id}>
+                                    <td><input type="radio" name="cliente_encontrado" value={cliente.id} onClick={this.selecionarCliente} /></td>
+                                    <td>{cliente.id}</td>
+                                    <td>{cliente.nome}</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+            )
+        } else {
+            return(
+                null
+            );
+        }
+    }
+
+    selecionarCliente = (e) => {
+        $('#modal-cliente').modal('hide');
+        this.input.current.value = e.target.value;
+        this.props.onChange(this.input.current.name, this.input.current.value);
+    }
+
     render() {
         return (
-            <input type="text" ref={this.input} name={this.props.name} id={this.props.id} className="form-control" readOnly={this.state.readonly} onChange={this.changeHandler} required />
+            <React.Fragment>
+                <input type="text" ref={this.input} name={this.props.name} id={this.props.id} className="form-control" readOnly={this.state.readonly} onChange={this.changeHandler} required />
+                <div className="modal fade" id="modal-cliente">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Procurar Cliente</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="row">
+                                    <div className="col-12">
+                                        <div className="form-group">
+                                            <label htmlFor="desejo">Você deseja buscar cliente por</label>
+                                            <select name="desejo" id="desejo" onChange={this.changeDesejo} className="form-control">
+                                                <option value="">&nbsp;</option>
+                                                <option value="id">Código</option>
+                                                <option value="cpfcnpj">CPF ou CNPJ</option>
+                                                <option value="nome">Nome</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="col-12">
+                                        {this.renderOptionsBusca()}
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        {this.renderClientesEncontrados()}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-nortelink" onClick={this.handleClienteClick}>Procurar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
         )
     }
 }
