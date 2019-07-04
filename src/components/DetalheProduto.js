96f46ff77a91fcb578ebd67930283e584ecca4e4
Ayron Felipe <ayron41@gmail.com>
2019-07-04 16:02:59 -0300

    Ajustes em detalhes de produtos
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/DetalheProduto.js
+++ src/components/DetalheProduto.js
@@ -88,113 +88,121 @@ export default class DetalheProduto extends React.Component {
                                                             </div>
                                                         </div>
                                                         <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cód. de Barras</h5>
                                                                 {this.state.produto.cod_barras == '' ? <input type="text" className="form-control" value="Não informado" readOnly={this.state.readOnly} /> : <input type="text" className="form-control" value={this.state.produto.cod_barras} readOnly={this.state.readOnly} /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cód. de integração</h5>
                                                                 {this.state.produto.cod_integ == '' ? <input type="text" className="form-control" value="Não informado" readOnly={this.state.readOnly} /> : <input type="text" className="form-control" value={this.state.produto.cod_integ} readOnly={this.state.readOnly} /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">% max de desconto</h5>
                                                                 <input type="text" value={`${this.state.produto.desc_maximo} %`} className="form-control" readOnly={this.state.readOnly} />
                                                             </div>
-                                                        </div>
-                                                        <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Embalagem fechada?</h5>
                                                                 {this.state.produto.emb_fechada == 0 ? <input type="text" value="Não" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value="Sim" className="form-control" readOnly={this.state.readOnly} /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Espécie</h5>
                                                                 {this.state.produto.especie == 'P' ? <input type="text" value="Produto" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value="Serviço" className="form-control" readOnly={this.state.readOnly} /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Fabricante</h5>
                                                                 {this.state.produto.fabricante == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value={this.state.produto.fabricante} className="form-control" readOnly={this.state.readOnly} /> }
                                                             </div>
                                                         </div>
                                                         <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cód. da Família</h5>
                                                                 {this.state.produto.familia == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value={this.state.produto.familia} className="form-control" readOnly={ this.state.readOnly } /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cód. da Loja</h5>
                                                                 {this.state.produto.id_loja == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value={this.state.produto.id_loja} className="form-control" readOnly={ this.state.readOnly } /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Marca</h5>
                                                                 {this.state.produto.marca == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value={this.state.produto.marca} className="form-control" readOnly={ this.state.readOnly } /> }
                                                             </div>
-                                                        </div>
-                                                        <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Nome da Loja</h5>
                                                                 {this.state.produto.nome_loja == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" value={this.state.produto.nome_loja} className="form-control" readOnly={ this.state.readOnly } /> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Peso Bruto</h5>
                                                                 <input type="text" value={`${this.state.produto.peso_bruto} kg` } className="form-control" readOnly={this.state.readOnly} />
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Peso Líquido</h5>
                                                                 <input type="text" value={`${this.state.produto.peso_liquido} kg` } className="form-control" readOnly={this.state.readOnly} />
                                                             </div>
                                                         </div>
                                                         <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Preço de Promoção</h5>
                                                                 <input type="text" value={this.state.produto.preco_promocao.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} readOnly={this.state.readOnly} className="form-control" />
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Preço de Venda</h5>
                                                                 <input type="text" value={this.state.produto.preco_venda.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })} readOnly={this.state.readOnly} className="form-control" />
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Qnt. na emb. de venda</h5>
                                                                 {this.state.produto.quant_emb == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.quant_emb} unidade`} ></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${ this.state.produto.quant_emb } unidades`}></input> }
                                                             </div>
-                                                        </div>
-                                                        <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Frações das und. de venda</h5>
                                                                 {this.state.produto.quant_fracoes == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.quant_fracoes} unidade`} ></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.quant_fracoes} unidades`} ></input>}
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cod. de ref. do fornecedor</h5>
                                                                 {this.state.produto.referencia == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={this.state.produto.referencia} ></input> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Disponível no depósito</h5>
                                                                 {this.state.produto.saldo_dep == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_dep} unidade`} ></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_dep} unidades`}></input>}
                                                             </div>
                                                         </div>
                                                         <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Estoque em frações disp.</h5>
                                                                 {this.state.produto.saldo_dep_frc == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_dep_frc} unidade`}></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_dep_frc} unidades`}></input>}
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Disponível para venda</h5>
                                                                 {this.state.produto.saldo_loja == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_loja} unidade`}></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_loja} unidades`}></input>}
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Frações disp. na loja</h5>
                                                                 {this.state.produto.saldo_loja_frc == 1 ? <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_loja_frc} unidade`}></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={`${this.state.produto.saldo_loja_frc} unidades`}></input>}
                                                             </div>
-                                                        </div>
-                                                        <div className="row">
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Status de venda</h5>
                                                                 {this.state.produto.status == 'A' ? <input type="text" className="form-control" readOnly={this.state.readOnly} value="Ativo" ></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value="Inativo"></input> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                        </div>
+                                                        <div className="row">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Cód. da tabela de tribut.</h5>
                                                                 {this.state.produto.tabela_trib == '' ? <input type="text" value="Não informado" className="form-control" readOnly={this.state.readOnly} /> : <input type="text" className="form-control" readOnly={this.state.readOnly} value={this.state.produto.tabela_trib}></input> }
                                                             </div>
-                                                            <div className="col-md-4 info-invoice">
+                                                            <div className="col-md-6 info-invoice">
                                                                 <h5 className="sub">Tipo</h5>
                                                                 {this.state.produto.tipo == 'Q' ? <input type="text" className="form-control" readOnly={this.state.readOnly} value="Quantidade" ></input> : <input type="text" className="form-control" readOnly={this.state.readOnly} value="Pesável" ></input> }
                                                             </div>
