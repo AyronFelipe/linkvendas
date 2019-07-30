425d4f5ab49cf94e09efeedc41d3688aa6b032a1
Ayron Felipe <ayron41@gmail.com>
2019-07-30 12:18:46 -0300

    Melhorias paginação
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,19 +5,42 @@ export default class Pagination extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = { ultima_pagina: false };
+        this.state = { ultima_pagina: false, pode_exibir: this.props.pode_exibir, nao_exibe_mais: false };
     }
 
     handleClick = (e) => {
+        let ante_penultima = Math.trunc(parseInt(this.props.count) / parseInt(this.props.rows));
+        let ultima = parseInt(ante_penultima) + 1;
+        this.setState({ ultima_pagina: false, pode_exibir: true, nao_exibe_mais: false });
         if (e.target.innerHTML == 'Anterior') {
-            this.props.onClick(parseInt(this.props.page) - 1);
+            let index = parseInt(this.props.page) - 1;
+            if (index == ante_penultima) {
+                this.setState({ nao_exibe_mais: true });
+            }
+            if (index == ultima) {
+                this.setState({ ultima_pagina: true, pode_exibir: false });
+            }
+            this.props.onClick(index);
         } else if (e.target.innerHTML == 'Próximo') {
-            this.props.onClick(parseInt(this.props.page) + 1);
+            let index = parseInt(this.props.page) + 1;
+            if (index == ante_penultima) {
+                this.setState({ nao_exibe_mais: true });
+            }
+            if (index == ultima) {
+                this.setState({ ultima_pagina: true, pode_exibir: false });
+            }
+            this.props.onClick(index);
         } else if (e.target.innerHTML == 'Primeira'){
             this.props.onClick(1);
         } else if (e.target.innerHTML == 'Última'){
-            let lol = Math.trunc(parseInt(this.props.count) / parseInt(this.props.rows));
-            this.props.onClick(parseInt(lol) + 1);
+            this.props.onClick(ultima);
+            this.setState({ ultima_pagina: true, pode_exibir: false });
+        } else if ( parseInt(e.target.innerHTML) == ultima ){
+            this.props.onClick(ultima);
+            this.setState({ ultima_pagina: true, pode_exibir: false });
+        } else if (parseInt(e.target.innerHTML) == ante_penultima) {
+            this.props.onClick(ante_penultima);
+            this.setState({ nao_exibe_mais: true });
         } else {
             this.props.onClick(e.target.innerHTML);
         }
@@ -45,24 +68,46 @@ export default class Pagination extends React.Component {
                                 :
                                     null
                             }
+                            {
+                                this.state.ultima_pagina ?
+                                <React.Fragment>
+                                    <li className="page-item">
+                                        <span className="page-link" onClick={this.handleClick}>
+                                            {parseInt(this.props.page) - 2}
+                                        </span>
+                                    </li>
+                                    <li className="page-item">
+                                        <span className="page-link" onClick={this.handleClick}>
+                                            {parseInt(this.props.page) - 1}
+                                        </span>
+                                    </li>
+                                </React.Fragment>
+                                :
+                                null
+                            }
                             <li className="page-item active">
                                 <span className="page-link" onClick={this.handleClick}>
                                     { parseInt(this.props.page) }
                                 </span>
                             </li>
                             {
-                                this.props.pode_exibir ?
+                                this.state.pode_exibir ?
                                     <React.Fragment>
                                         <li className="page-item">
                                             <span className="page-link" onClick={this.handleClick}>
                                                 { parseInt(this.props.page) + 1 }
                                             </span>
                                         </li>
-                                        <li className="page-item">
-                                            <span className="page-link" onClick={this.handleClick}>
-                                                { parseInt(this.props.page) + 2 }
-                                            </span>
-                                        </li>
+                                        {
+                                            this.state.nao_exibe_mais ?
+                                            null
+                                            :
+                                            <li className="page-item">
+                                                <span className="page-link" onClick={this.handleClick}>
+                                                    { parseInt(this.props.page) + 2 }
+                                                </span>
+                                            </li>
+                                        }
                                         <li className="page-item">
                                             <span className="page-link" onClick={this.handleClick}>
                                                 Próximo
